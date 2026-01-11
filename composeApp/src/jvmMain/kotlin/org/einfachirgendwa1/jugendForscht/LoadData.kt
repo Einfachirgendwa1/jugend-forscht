abdefe78044228706d5464905f84de17c333a8cc
Einfachirgendwa1 <153438063+Einfachirgendwa1@users.noreply.github.com>
2026-01-11 20:33:29 +0100

    commit

--- composeApp/src/jvmMain/kotlin/org/einfachirgendwa1/jugendForscht/LoadData.kt
+++ composeApp/src/jvmMain/kotlin/org/einfachirgendwa1/jugendForscht/LoadData.kt
@@ -4,13 +4,14 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.withContext
 import java.sql.DriverManager
+import java.sql.ResultSet
 
-// TODO
+// TODO: implement receiver path
 fun pathToReceiver(): String {
     return "C:\\Users\\Think\\IdeaProjects\\JugendForscht\\jugend-forscht-receiver\\target\\release\\jugend-forscht-receiver.exe"
 }
 
-// TODO
+// TODO: implement database path
 fun pathToDb(): String {
     return "C:\\Users\\Think\\IdeaProjects\\JugendForscht\\main.db"
 }
@@ -19,8 +20,8 @@ suspend fun loadData(sensors: List<Sensor>) {
     val receiverPath = pathToReceiver()
     val dbPath = pathToDb()
 
-    withContext(Dispatchers.IO) {
-        ProcessBuilder(receiverPath, dbPath).inheritIO().start()
+    val child = withContext(Dispatchers.IO) {
+        ProcessBuilder(receiverPath, dbPath, "--spoof", "--reset-data").inheritIO().start()
     }
 
     val connection = DriverManager.getConnection("jdbc:sqlite:$dbPath")
@@ -52,14 +53,22 @@ suspend fun loadData(sensors: List<Sensor>) {
 
     onAppClose.add { select.close() }
     onAppClose.add { connection.close() }
+    onAppClose.add { child.destroy() }
 
     while (true) {
         val result = select.executeQuery()
         while (result.next()) {
-            sensors.find { it.id == result.getInt("sensor") }
-                ?.let { it.pegel = result.getInt("value").toDouble().also { println("Setting pegel to $it") } }
+            val sensor = DbSensor(result)
+            sensors.find { it.sensorId == sensor.sensor }?.let { it.pegel = sensor.value.toDouble() / 1000 }
         }
 
-        delay(1000)
+        delay(50)
     }
+}
+
+class DbSensor(resultSet: ResultSet) {
+    val id: Int = resultSet.getInt("id")
+    val sensor: Int = resultSet.getInt("sensor")
+    val timestamp: Int = resultSet.getInt("timestamp")
+    val value: Int = resultSet.getInt("value")
 }
